2a4eff82e31c5aabc31d4b103946d2933479c62e
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-11-09 21:44:11 -0300

    Fix FrustrumCulling

--- TP4Algebra/Assets/Scripts/FrustrumCulling.cs
+++ TP4Algebra/Assets/Scripts/FrustrumCulling.cs
@@ -6,96 +6,71 @@ using UnityEngine;
 public class FrustrumCulling : MonoBehaviour
 {
     [SerializeField] private float fov;
-    [SerializeField] private float distance;
-    [SerializeField] private float minDistance;
+    [SerializeField] private float farDistance;
+    [SerializeField] private float nearDistance;
+    [SerializeField] private float size;
+    [SerializeField] private float tanAngle;
 
     [SerializeField] private Vector3 sizeP1;
     [SerializeField] private Vector3 sizeP2;
     [SerializeField] private Vector2 aspectRatio;
 
-    [SerializeField] private Vector3 centerOfScreen;
-    [SerializeField] private Vector3 centerOfScreen2;
-
-    [SerializeField] private Vector3[] vertices;
-
-    [SerializeField] private float tanAngle;
-
     [SerializeField] private List<GameObject> listObj = new List<GameObject>();
-    [SerializeField] private List<Plane> planes = new List<Plane>();
+    [SerializeField] private List<MyPlane> planes = new List<MyPlane>();
 
-    private void Awake()
+    private void OnEnable()
     {
-        vertices = new Vector3[8];
+        Vector3 frontNear = nearDistance * transform.forward;
+        Vector3 frontFar = farDistance * transform.forward;
 
-        Plane newP = new Plane(vertices[0], vertices[1], vertices[2], vertices[3]); // Front
-        planes.Add(newP);
+        tanAngle = Mathf.Deg2Rad * fov;
+        float vertical = farDistance * Mathf.Tan(tanAngle * 0.5f);
+        float horizontal = vertical * aspectRatio.y / aspectRatio.x;
 
-        Plane newP2 = new Plane(vertices[4], vertices[0], vertices[3], vertices[7]); // Left Plane
-        planes.Add(newP2);
+        MyPlane near = new MyPlane(transform.position + frontFar, transform.forward);
+        MyPlane far = new MyPlane(transform.position + frontNear, -transform.forward);
+        MyPlane right = new MyPlane(transform.position, Vector3.Cross(transform.up, frontNear + transform.right * horizontal));
+        MyPlane left = new MyPlane(transform.position, Vector3.Cross(frontNear - transform.right * horizontal, transform.up));
+        MyPlane top = new MyPlane(transform.position, Vector3.Cross(transform.right, frontNear - transform.up * vertical));
+        MyPlane bottom = new MyPlane(transform.position, Vector3.Cross(frontNear + transform.up * vertical, transform.right));
 
-        Plane newP3 = new Plane(vertices[1], vertices[5], vertices[6], vertices[2]); // Right Plane
-        planes.Add(newP3);
-
-        Plane newP4 = new Plane(vertices[4], vertices[5], vertices[1], vertices[0]); // Top Plane
-        planes.Add(newP4);
-
-        Plane newP5 = new Plane(vertices[3], vertices[2], vertices[6], vertices[7]); // Down Plane
-        planes.Add(newP5);
-
-        Plane newP6 = new Plane(vertices[7], vertices[6], vertices[5], vertices[4]); // Back Plane
-        planes.Add(newP6);
+        planes.Add(near);
+        planes.Add(far);
+        planes.Add(right);
+        planes.Add(left);
+        planes.Add(top);
+        planes.Add(bottom);
     }
 
-    // Update is called once per frame
     void Update()
     {
         tanAngle = Mathf.Deg2Rad * fov;
 
-        centerOfScreen = transform.position + (transform.forward * minDistance);
-        centerOfScreen2 = transform.position + (transform.forward * distance);
-
-        sizeP1.x = minDistance * Mathf.Tan(tanAngle / 2f);
-        sizeP1.y = sizeP1.x * aspectRatio.y / aspectRatio.x;
-
-        sizeP2.x = distance * Mathf.Tan(tanAngle / 2f);
-        sizeP2.y = sizeP2.x * aspectRatio.y / aspectRatio.x;
-
-        CalculateVertices();
-        UpdateVerticesInPlanes();
+        UpdatePlanes();
 
         Culling();
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.yellow;
-
-        for (int i = 0; i < planes.Count; i++)
-        {
-            planes[i].DrawGizmo(transform);
-        }
+        DrawFrustrum(planes);
     }
 
-    void CalculateVertices()
+    void UpdatePlanes()
     {
-        vertices[0] = transform.up * sizeP1.y + transform.right * -sizeP1.x + centerOfScreen;
-        vertices[1] = transform.up * sizeP1.y + transform.right * sizeP1.x + centerOfScreen;
-        vertices[2] = transform.up * -sizeP1.y + transform.right * sizeP1.x + centerOfScreen;
-        vertices[3] = transform.up * -sizeP1.y + transform.right * -sizeP1.x + centerOfScreen;
+        Vector3 frontFar = nearDistance * transform.forward;
+        Vector3 frontNear = farDistance * transform.forward;
 
-        vertices[4] = transform.up * sizeP2.y + transform.right * -sizeP2.x + centerOfScreen2;
-        vertices[5] = transform.up * sizeP2.y + transform.right * sizeP2.x + centerOfScreen2;
-        vertices[6] = transform.up * -sizeP2.y + transform.right * sizeP2.x + centerOfScreen2;
-        vertices[7] = transform.up * -sizeP2.y + transform.right * -sizeP2.x + centerOfScreen2;
-    }
-    void UpdateVerticesInPlanes()
-    {
-        planes[0].SetVertices(vertices[0], vertices[1], vertices[2], vertices[3]); // Front
-        planes[1].SetVertices(vertices[4], vertices[0], vertices[3], vertices[7]); // Left
-        planes[2].SetVertices(vertices[1], vertices[5], vertices[6], vertices[2]); // Right
-        planes[3].SetVertices(vertices[4], vertices[5], vertices[1], vertices[0]); // Top
-        planes[4].SetVertices(vertices[3], vertices[2], vertices[6], vertices[7]); // Down
-        planes[5].SetVertices(vertices[7], vertices[6], vertices[5], vertices[4]); // Back
+        tanAngle = Mathf.Deg2Rad * fov;
+        float horizontal = nearDistance * Mathf.Tan(tanAngle * 0.5f);
+        float vertical = horizontal * aspectRatio.y / aspectRatio.x;
+
+        planes[0].SetNormalAndPosition(transform.position + frontNear, transform.forward);
+        planes[1].SetNormalAndPosition(transform.position + frontFar, -transform.forward);
+        planes[2].SetNormalAndPosition(transform.position, Vector3.Cross(transform.up, frontFar + transform.right * horizontal));
+        planes[3].SetNormalAndPosition(transform.position, Vector3.Cross(frontFar - transform.right * horizontal, transform.up));
+        planes[4].SetNormalAndPosition(transform.position, Vector3.Cross(transform.right, frontFar - transform.up * vertical));
+        planes[5].SetNormalAndPosition(transform.position, Vector3.Cross(frontFar + transform.up * vertical, transform.right));
     }
 
     void Culling()
@@ -115,7 +90,7 @@ public class FrustrumCulling : MonoBehaviour
 
                     for (int k = 0; k < planes.Count; k++)
                     {
-                        if (planes[k].IsPositiveToThePlane(aabb.BoxVertices[j]))
+                        if (planes[k].GetSide(aabb.BoxVertices[j]))
                             isPosAllPlanes--;
                     }
 
@@ -130,4 +105,49 @@ public class FrustrumCulling : MonoBehaviour
             }
         }
     }
+
+    private void DrawFrustrum(List<MyPlane> frustumPlanes) // Chat GPT ME AYUDO CON EL DIBUJADO
+    {
+        if (frustumPlanes.Count != 6) return;
+
+        Vector3[] corners = new Vector3[8];
+        corners[0] = IntersectThreePlanes(frustumPlanes[0], frustumPlanes[2], frustumPlanes[4]); // Near Top Left
+        corners[1] = IntersectThreePlanes(frustumPlanes[1], frustumPlanes[2], frustumPlanes[4]); // Near Top Right
+        corners[2] = IntersectThreePlanes(frustumPlanes[1], frustumPlanes[3], frustumPlanes[4]); // Near Bottom Right
+        corners[3] = IntersectThreePlanes(frustumPlanes[0], frustumPlanes[3], frustumPlanes[4]); // Near Bottom Left
+        corners[4] = IntersectThreePlanes(frustumPlanes[0], frustumPlanes[2], frustumPlanes[5]); // Far Top Left
+        corners[5] = IntersectThreePlanes(frustumPlanes[1], frustumPlanes[2], frustumPlanes[5]); // Far Top Right
+        corners[6] = IntersectThreePlanes(frustumPlanes[1], frustumPlanes[3], frustumPlanes[5]); // Far Bottom Right
+        corners[7] = IntersectThreePlanes(frustumPlanes[0], frustumPlanes[3], frustumPlanes[5]); // Far Bottom Left
+
+        Gizmos.color = Color.green;
+        for (int i = 0; i < 4; i++)
+        {
+            Gizmos.DrawLine(corners[i], corners[(i + 1) % 4]);
+            Gizmos.DrawLine(corners[i + 4], corners[(i + 1) % 4 + 4]);
+            Gizmos.DrawLine(corners[i], corners[i + 4]);
+        }
+    }
+
+    private Vector3 IntersectThreePlanes(MyPlane p1, MyPlane p2, MyPlane p3)
+    {
+        Vector3 n1 = p1.Normal, n2 = p2.Normal, n3 = p3.Normal;
+        float determinant = Vector3.Dot(n1, Vector3.Cross(n2, n3));
+
+        if (Mathf.Abs(determinant) < 1e-6f)
+        {
+            Debug.LogWarning("Planes do not intersect at a single point.");
+            return Vector3.zero;
+        }
+
+        Vector3 intersectPoint = (
+            (-p1.Distance * Vector3.Cross(n2, n3)) +
+            (-p2.Distance * Vector3.Cross(n3, n1)) +
+            (-p3.Distance * Vector3.Cross(n1, n2))
+        ) / determinant;
+
+        return intersectPoint;
+    }
 }
+
+
